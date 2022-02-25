f7a282fbb52d2dadfbe8f80df3ae2dbfd78b92bf
Damaris <damarisoliveira@users.noreply.github.com>
2022-02-25 12:44:19 -0300

    Commitando o DomainException, adicionando o rastreador de compatibilidade de versoes

--- src/exceptions/DomainException.java
+++ src/exceptions/DomainException.java
@@ -2,6 +2,8 @@ package exceptions;
 
 public class DomainException extends RuntimeException {
 	
+	private static final long serialVersionUID = 1L;
+	//  serve para rastrear a compatibilidade de versões serializadas das classes
 	public DomainException(String mensagem) {
 		super(mensagem);
 	}
